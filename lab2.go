409b6a19e2f101013625d58e406813d18ab4275f
shreyastawre <tawre.shreyas@gmail.com>
2018-03-02 05:44:54 -0600

    Testing sequential again

--- lab2.go
+++ lab2.go
@@ -100,43 +100,43 @@ func hash(hash uint64, val int) uint64 {
 	return (hash * uint64(val2) + uint64(val2)) % prime
 }
 
-func hashFunc(tree []int, bst_id int) {
+// func hashFunc(tree []int, bst_id int) {
 // func hashFunc(tree []int, my_chan *chan map_element, bst_id int) {
-// func hashFunc(tree []int, hashI *uint64) {
+func hashFunc(tree []int, hashI *uint64) {
 	var retval uint64 = 0
 	tree_len := len(tree)
 	for i := 0; i < tree_len; i++ {
 		retval = hash(retval, tree[i])
 	}
 
-	// *hashI = retval
+	*hashI = retval
 
 	// *my_chan <- map_element{hash: retval, bst_id: bst_id}
-	mux.Lock()
-	hash_map[retval] = append(hash_map[retval], bst_id)
-	mux.Unlock()
+	// mux.Lock()
+	// hash_map[retval] = append(hash_map[retval], bst_id)
+	// mux.Unlock()
 
 	// wg.Done()
 }
 
-func parallelHashFunc(partition *[][]int, q int, wg *sync.WaitGroup, i int) {
-// func parallelHashFunc(partition *[][]int, q int, tree_hash *uint64, wg *sync.WaitGroup) {
-// func parallelHashFunc(partition *[][]int, q int, wg *sync.WaitGroup, my_chan *chan map_element, i int) {
-
-	// fmt.Println(q)
-	for j := 0; j < q; j++ {
-		// fmt.Println(i, j)
-		// wg.Add(1)
-
-		bst_id := q*i + j
-
-		hashFunc((*partition)[j], bst_id)
-		// hashFunc((*partition)[j], my_chan, bst_id)
-		if (j == q - 1) {
-			wg.Done()
-		}
-	}
-}
+// func parallelHashFunc(partition *[][]int, q int, wg *sync.WaitGroup, i int) {
+// // func parallelHashFunc(partition *[][]int, q int, tree_hash *uint64, wg *sync.WaitGroup) {
+// // func parallelHashFunc(partition *[][]int, q int, wg *sync.WaitGroup, my_chan *chan map_element, i int) {
+//
+// 	// fmt.Println(q)
+// 	for j := 0; j < q; j++ {
+// 		// fmt.Println(i, j)
+// 		// wg.Add(1)
+//
+// 		bst_id := q*i + j
+//
+// 		hashFunc((*partition)[j], bst_id)
+// 		// hashFunc((*partition)[j], my_chan, bst_id)
+// 		if (j == q - 1) {
+// 			wg.Done()
+// 		}
+// 	}
+// }
 
 func compareTrees(tree1 []int, tree2 []int) bool {
 	size := len(tree1)
@@ -151,17 +151,17 @@ func compareTrees(tree1 []int, tree2 []int) bool {
 	return true
 }
 
-func parallelCompareTrees(my_buffer *chan tree_pair, wg *sync.WaitGroup) {
-	for buffer_counter > 0 {
-		my_pair := <- *my_buffer
-		buffer_counter--
-		my_cond.Signal()
-		if compareTrees(inOrderTrees[my_pair.bst_id1], inOrderTrees[my_pair.bst_id2]) {
-			equality[my_pair.bst_id1][my_pair.bst_id2] = true
-		}
-	}
-	wg.Done()
-}
+// func parallelCompareTrees(my_buffer *chan tree_pair, wg *sync.WaitGroup) {
+// 	for buffer_counter > 0 {
+// 		my_pair := <- *my_buffer
+// 		buffer_counter--
+// 		my_cond.Signal()
+// 		if compareTrees(inOrderTrees[my_pair.bst_id1], inOrderTrees[my_pair.bst_id2]) {
+// 			equality[my_pair.bst_id1][my_pair.bst_id2] = true
+// 		}
+// 	}
+// 	wg.Done()
+// }
 
 func main() {
 	hashWorkers := flag.Int("hash-workers", 1, "hash workers")
@@ -222,7 +222,7 @@ func main() {
 		inOrderTrees[i] = inOrderTraversal(trees[i])
 	}
 
-	// tree_hashes := make([]uint64, tree_size)
+	tree_hashes := make([]uint64, tree_size)
 	// tree_map := make(map[int][]int)
 
 	var wg sync.WaitGroup
@@ -234,10 +234,11 @@ func main() {
 
 	// start1 := time.Now()
 	//
-	// for i := 0; i < tree_size; i++ {
-	// 	wg.Add(1)
-	// 	go hashFunc(trees[i], &tree_hashes[i], &wg)
-	// }
+	for i := 0; i < tree_size; i++ {
+		// wg.Add(1)
+		// go hashFunc(trees[i], &tree_hashes[i], &wg)
+		hashFunc(trees[i], &tree_hashes[i])
+	}
 	//
 	// wg.Wait()
 	//
@@ -245,47 +246,47 @@ func main() {
 
 	/* Thread pool implementation */
 
-	q := tree_size / *hashWorkers
-	// fmt.Println(q)
-	r := tree_size % *hashWorkers
-
-	trees_partitions := make([][][]int, *hashWorkers)
-	for i := range trees_partitions {
-		trees_partitions[i] = make([][]int, q)
-		for j := range trees_partitions[i] {
-			trees_partitions[i][j] = make([]int, tree_dim)
-		}
-	}
-
-	counter := 0
-	for i := 0; i < *hashWorkers; i++ {
-		for j := 0; j < q; j++ {
-			for k := 0; k < tree_dim; k++ {
-				trees_partitions[i][j][k] = trees[counter][k]
-			}
-			counter++
-		}
-	}
-
-	for i := 0; i < r; i++ {
-		trees_partitions[i] = append(trees_partitions[i], trees[counter])
-		counter++
-	}
-
-	start1 := time.Now()
-
-	// my_chan := make(chan map_element)
-
-	// c2 := 0
-	for i := 0; i < *hashWorkers; i++ {
-		wg.Add(1)
-		// go parallelHashFunc(&trees_partitions[i], q, &wg, &my_chan, i)
-		go parallelHashFunc(&trees_partitions[i], q, &wg, i)
-		// go parallelHashFunc(partition, q, &tree_hashes[i], &wg, i)
-		// c2++
-	}
-
-	wg.Wait()
+	// q := tree_size / *hashWorkers
+	// // fmt.Println(q)
+	// r := tree_size % *hashWorkers
+	//
+	// trees_partitions := make([][][]int, *hashWorkers)
+	// for i := range trees_partitions {
+	// 	trees_partitions[i] = make([][]int, q)
+	// 	for j := range trees_partitions[i] {
+	// 		trees_partitions[i][j] = make([]int, tree_dim)
+	// 	}
+	// }
+	//
+	// counter := 0
+	// for i := 0; i < *hashWorkers; i++ {
+	// 	for j := 0; j < q; j++ {
+	// 		for k := 0; k < tree_dim; k++ {
+	// 			trees_partitions[i][j][k] = trees[counter][k]
+	// 		}
+	// 		counter++
+	// 	}
+	// }
+	//
+	// for i := 0; i < r; i++ {
+	// 	trees_partitions[i] = append(trees_partitions[i], trees[counter])
+	// 	counter++
+	// }
+	//
+	// start1 := time.Now()
+	//
+	// // my_chan := make(chan map_element)
+	//
+	// // c2 := 0
+	// for i := 0; i < *hashWorkers; i++ {
+	// 	wg.Add(1)
+	// 	// go parallelHashFunc(&trees_partitions[i], q, &wg, &my_chan, i)
+	// 	go parallelHashFunc(&trees_partitions[i], q, &wg, i)
+	// 	// go parallelHashFunc(partition, q, &tree_hashes[i], &wg, i)
+	// 	// c2++
+	// }
+	//
+	// wg.Wait()
 	// hash_map := make(map[uint64][]int)
 
 	 // go func (my_chan chan map_element){
@@ -294,50 +295,51 @@ func main() {
 		// }
 	 // }(my_chan)
 
-	// for i := 0; i < tree_size; i++ {
-	// 	for j := 0; j < tree_size; j++ {
-	// 		if (tree_hashes[i] == tree_hashes[j]) {
-	// 			equality[i][j] = true
-	// 			if compareTrees(inOrderTrees[i], inOrderTrees[j]) {
-	// 				tree_map[i] = append(tree_map[i], j)
-	// 			}
-	// 		}
-	// 	}
-	// }
-	var wg1 sync.WaitGroup
-	my_buffer := make(chan tree_pair)
-	my_cond = sync.NewCond(&mux)
-
-	go func (my_buffer *chan tree_pair) {
-		for elem := range hash_map {
-			temp := hash_map[elem]
-			n := len(temp)
-			if (n > 1) {
-				for id1 := range temp {
-					for id2 := range temp {
-						// go compareTrees(inOrderTrees[id1], inOrderTrees[id2], &wg1, &retval)
-						for buffer_counter == *compWorkers {
-							my_cond.Wait()
-						}
-						*my_buffer <- tree_pair{bst_id1: id1, bst_id2: id2}
-						buffer_counter++
-					}
+	for i := 0; i < tree_size; i++ {
+		for j := 0; j < tree_size; j++ {
+			if (tree_hashes[i] == tree_hashes[j]) {
+				// equality[i][j] = true
+				if compareTrees(inOrderTrees[i], inOrderTrees[j]) {
+					equality[i][j] = true
+					// tree_map[i] = append(tree_map[i], j)
 				}
-			} else {
-				 equality[temp[0]][temp[0]] = true
 			}
 		}
-	} (&my_buffer)
-
-	for i := 0; i < *compWorkers; i++ {
-		wg1.Add(1)
-		// go parallelHashFunc(&trees_partitions[i], q, &wg, &my_chan, i)
-		go parallelCompareTrees(&my_buffer, &wg1)
-		// go parallelHashFunc(partition, q, &tree_hashes[i], &wg, i)
-		// c2++
 	}
-
-	wg1.Wait()
+	// var wg1 sync.WaitGroup
+	// my_buffer := make(chan tree_pair)
+	// my_cond = sync.NewCond(&mux)
+	//
+	// go func (my_buffer *chan tree_pair) {
+	// 	for elem := range hash_map {
+	// 		temp := hash_map[elem]
+	// 		n := len(temp)
+	// 		if (n > 1) {
+	// 			for id1 := range temp {
+	// 				for id2 := range temp {
+	// 					// go compareTrees(inOrderTrees[id1], inOrderTrees[id2], &wg1, &retval)
+	// 					for buffer_counter == *compWorkers {
+	// 						my_cond.Wait()
+	// 					}
+	// 					*my_buffer <- tree_pair{bst_id1: id1, bst_id2: id2}
+	// 					buffer_counter++
+	// 				}
+	// 			}
+	// 		} else {
+	// 			 equality[temp[0]][temp[0]] = true
+	// 		}
+	// 	}
+	// } (&my_buffer)
+	//
+	// for i := 0; i < *compWorkers; i++ {
+	// 	wg1.Add(1)
+	// 	// go parallelHashFunc(&trees_partitions[i], q, &wg, &my_chan, i)
+	// 	go parallelCompareTrees(&my_buffer, &wg1)
+	// 	// go parallelHashFunc(partition, q, &tree_hashes[i], &wg, i)
+	// 	// c2++
+	// }
+	//
+	// wg1.Wait()
 	elapsed1 := time.Since(start1)
 
 	fmt.Printf("Time taken by 1: %s\n", elapsed1)
